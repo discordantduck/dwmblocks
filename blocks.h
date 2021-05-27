2151a755987be84499722bfcd4bae7dfa3f4c3c9
discordantduck <>
2021-05-27 04:42:37 -0700

    ...

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] =
 {
-/*  ICON        COMMAND                                 UPDATE INTERVAL UPDATE SIGNAL */
-    {" WIFI[",  "./dwmblocks/scripts/network",          60,             0},
-    {" VOL[",   "./dwmblocks/scripts/outputVolume",     0,              10},
-    {" MIC[",   "./dwmblocks/scripts/inputVolume",      0,              11},
-    {" ",       "./dwmblocks/scripts/date",             60,             0},
+/*  ICON        COMMAND                                         UPDATE INTERVAL     UPDATE SIGNAL */
+    {" WIFI[",  "~/Desktop/dwmblocks/scripts/network",          60,                 0},
+    {" VOL[",   "~/Desktop/dwmblocks/scripts/outputVolume",     0,                  10},
+    {" MIC[",   "~/Desktop/dwmblocks/scripts/inputVolume",      0,                  11},
+    {" ",       "~/Desktop/dwmblocks/scripts/date",             60,                 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
